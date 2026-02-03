c46d56b82118d1d87247149e7a7ff3b896a26ee4
John Arthur Badiango <johnarthurbadiango3011@gmail.com>
2026-02-03 19:39:40 +0800

    Created mysql connection to database, created initial tables

--- src/main/resources/db/migration/V1__create_timely_management_tables.sql
+++ src/main/resources/db/migration/V1__create_timely_management_tables.sql
@@ -15,10 +15,39 @@ CREATE table user_level(
 
 CREATE table user_profile(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     middle_name VARCHAR(100) NULL,
     address varchar(200) NOT NULL,
     profile_picture VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE table company(
+
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    company_name VARCHAR(50) NOT NULL,
+    company_address VARCHAR(100) NOT NULL,
+    company_admin_email VARCHAR(50) NOT NULL,
+    is_verified BOOLEAN NOT NULL,
+    is_subscribed BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+
+);
+
+CREATE table time_log(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    time_in TIMESTAMP NOT NULL,
+    time_out TIMESTAMP DEFAULT NULL,
+    is_night_shift BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE table time_log_pause(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    time_log_id BIGINT NOT NULL,
+    time_resume TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
